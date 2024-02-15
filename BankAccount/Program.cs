62b3c856d38fbc7efc2a23c1e3cd2ab005d07fb0
VladProgrammer08 <bumbuv136@gmail.com>
2024-02-14 19:26:14 -0800

    Added some test code

--- BankAccount/Program.cs
+++ BankAccount/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BankAccount;
+
+Account myAccount = new("Vlad B");
+myAccount.Deposit(500);
